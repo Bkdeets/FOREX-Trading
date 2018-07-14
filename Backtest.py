2bd9b0fd5cb171921c203336dc4d708a7ca1ace3
Britton <brittonkdeets@gmail.com>
2018-07-14 16:05:19 -0400

    Testing to see how committing works with git

--- Backtest.py
+++ Backtest.py
@@ -1,3 +1,15 @@
 ## Author: Britton K. Deets
 ## Created: July 14th , 2018
 ## Version: 1
+
+
+# change made
+
+def backtest(function, dataset):
+    return None
+
+
+def add(x,y):
+    print(x+y)
+
+add(9,10)
